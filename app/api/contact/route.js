33c39286b6b1b93e7ebd1de383b31cf30f50d296
CodeWithAnkit12 <ankitsingh10665@gmail.com>
2025-08-06 00:26:57 +0530

    fdvdf

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,11 +1,5 @@
-import axios from 'axios';
 import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
-import { useEffect } from "react";
-
-useEffect(() => {
-  console.log("Running only in browser:", document.title);
-}, []);
 
 // Create and configure Nodemailer transporter
 const transporter = nodemailer.createTransport({
